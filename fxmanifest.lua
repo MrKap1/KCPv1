b86ac716068e9babf171c62eadd97b233e669ef6
Kap <karolgrz32@gmail.com>
2025-12-09 23:22:42 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,6 +10,10 @@ files {
   'first_person_alternates.meta'
 }
 
+server_scripts {
+    'server.lua',
+}
+
 data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_kcpv1.meta'
 data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_kcpv1.meta'
 data_file 'ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
