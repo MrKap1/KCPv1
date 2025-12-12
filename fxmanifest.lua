de4c09a83e24ab694161b7768b805be6fe3e1e4b
MrKap1 <karolgrz32@gmail.com>
2025-12-12 00:04:51 -0600

    Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,22 +1,24 @@
 author 'Kap'
 description 'Kap Custom Clothing Pack V1'
-version '1.0.0'
+version '1.0.5'
 lua54 'yes'
 fx_version 'cerulean'
 game { 'gta5' }
 
-files {
-  'mp_m_freemode_01_mp_m_kcpv1.meta',
-  'mp_f_freemode_01_mp_f_kcpv1.meta',
-  'pedalternatevariations.meta',
-  'first_person_alternates.meta'
-}
+
 
 server_scripts {
     'server.lua',
 }
 
-data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_kcpv1.meta'
-data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_kcpv1.meta'
+files {
+  'mp_m_freemode_01_mp_m_KCPv1_shop.meta',
+  'mp_f_freemode_01_mp_f_KCPv1_shop.meta',
+  'pedalternatevariations.meta',
+  'first_person_alternates.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_KCPv1_shop.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_KCPv1_shop.meta'
 data_file 'ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
 data_file 'PED_FIRST_PERSON_ALTERNATE_DATA' 'first_person_alternates.meta'
